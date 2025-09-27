f12da7470fd0c3da94ee7fb7d45c6498b80651ec
Mossab Idbenkellal <idmossab@gmail.com>
2025-09-27 03:13:25 +0100

    orderbook

---
+++ order_books/src/lib.rs
@@ -0,0 +1,20 @@
+pub mod library{
+    pub mod writers{
+         use crate::library::books::Book;
+        pub struct Writer {
+           pub first_name: String,
+           pub last_name: String,
+           pub books: Vec<Book>,
+        }
+    }
+    pub mod books{
+        pub struct Book {
+           pub title: String,
+           pub year: u64,
+        }
+    }
+}
+use crate::library::writers::Writer;
+pub fn order_books(writer: &mut Writer) {
+    writer.books.sort_by_key(|book| book.title.clone())        
+}
