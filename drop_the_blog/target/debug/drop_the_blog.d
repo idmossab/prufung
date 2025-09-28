dc5c80c707c465911e254adf141424257e71105b
Mossab Idbenkellal <idmossab@gmail.com>
2025-09-28 08:20:13 +0100

    drop the blog

--- drop_the_blog/target/debug/drop_the_blog.d
+++
@@ -1,1 +0,0 @@
-/home/midbenke/Desktop/prufung/drop_the_blog/target/debug/drop_the_blog: /home/midbenke/Desktop/prufung/drop_the_blog/src/lib.rs /home/midbenke/Desktop/prufung/drop_the_blog/src/main.rs
